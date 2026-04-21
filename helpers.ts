4d701c2735027c5261842b89ac3ffa4096143f42
testezhin12-jpg <testezhin12@gmail.com>
2026-04-21 20:15:18 +0100

    test:implement type annotations and docstrings

--- helpers.ts
+++ helpers.ts
@@ -1,25 +1,47 @@
-export function isEmpty(obj: any): boolean {
-    return Object.keys(obj).length === 0;
+/**
+ * Checks if a value is an array.
+ * @param value - The value to check.
+ * @returns True if the value is an array, else false.
+ */
+function isArray(value: any): value is Array<any> {
+    return Array.isArray(value);
 }
 
-export function deepClone<T>(obj: T): T {
-    return JSON.parse(JSON.stringify(obj));
-}
-
-export function mergeDeep<T>(target: T, source: Partial<T>): T {
+/**
+ * Merges two objects deeply.
+ * @param target - The target object to merge into.
+ * @param source - The source object to merge from.
+ * @returns The merged object.
+ */
+function deepMerge<T extends object, U extends object>(
+    target: T,
+    source: U
+): T & U {
     for (const key in source) {
-        if (source[key] instanceof Object && key in target) {
-            Object.assign(source[key], mergeDeep(target[key], source[key]));
+        if (source.hasOwnProperty(key)) {
+            const value = source[key];
+            if (isArray(value)) {
+                target[key] = [...(target[key] as any) || [], ...value];
+            } else if (value && typeof value === 'object') {
+                target[key] = deepMerge(target[key] as any, value);
+            } else {
+                target[key] = value;
+            }
         }
     }
-    Object.assign(target || {}, source);
-    return target;
+    return target as T & U;
 }
 
-export function debounce(fn: Function, delay: number) {
-    let timeoutId: NodeJS.Timeout;
-    return (...args: any[]) => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => fn(...args), delay);
-    };
+/**
+ * Generates a random UUID.
+ * @returns A random UUID string.
+ */
+function generateUUID(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+        .replace(/[xy]/g, function(c) {
+            const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r % 4 + 8);
+            return v.toString(16);
+        });
 }
+
+export { isArray, deepMerge, generateUUID };
