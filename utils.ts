1402fa77ff52a60210fca9c18820d7073b325532
testezhin12-jpg <testezhin12@gmail.com>
2026-04-21 20:16:18 +0100

    test:implement data handling utility functions

--- utils.ts
+++ utils.ts
@@ -1,34 +1,37 @@
-function debounce(func: Function, wait: number) {
-    let timeout: NodeJS.Timeout;
-    return function executedFunction(...args: any) {
-        const later = () => {
-            clearTimeout(timeout);
-            func(...args);
-        };
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-    };
+type Data = Record<string, any>;
+
+type FilterFunction<T> = (item: T) => boolean;
+
+type SortFunction<T> = (a: T, b: T) => number;
+
+/**
+ * Filters an array of data based on a filter function.
+ * @param data - The array of data to be filtered.
+ * @param filter - A function to test each item.
+ * @returns A new array containing items that pass the filter.
+ */
+function filterData<T>(data: T[], filter: FilterFunction<T>): T[] {
+    return data.filter(filter);
 }
 
-function throttle(func: Function, limit: number) {
-    let lastFunc: NodeJS.Timeout;
-    let lastRan: number;
-    return function() {
-        const context = this;
-        const args = arguments;
-        if (!lastRan) {
-            func.apply(context, args);
-            lastRan = Date.now();
-        } else {
-            clearTimeout(lastFunc);
-            lastFunc = setTimeout(function() {
-                if ((Date.now() - lastRan) >= limit) {
-                    func.apply(context, args);
-                    lastRan = Date.now();
-                }
-            }, limit - (Date.now() - lastRan));
-        }
-    };
+/**
+ * Sorts an array of data based on a sort function.
+ * @param data - The array of data to be sorted.
+ * @param sort - A function that defines the sort order.
+ * @returns A new sorted array.
+ */
+function sortData<T>(data: T[], sort: SortFunction<T>): T[] {
+    return [...data].sort(sort);
 }
 
-export { debounce, throttle };
+/**
+ * Merges two arrays into one, excluding duplicates.
+ * @param array1 - The first array to merge.
+ * @param array2 - The second array to merge.
+ * @returns A new array with unique items from both arrays.
+ */
+function mergeUnique<T>(array1: T[], array2: T[]): T[] {
+    return Array.from(new Set([...array1, ...array2]));
+}
+
+export { filterData, sortData, mergeUnique, Data };
